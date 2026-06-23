fc31cba3acf098e34fbc4a778c70e95453d5e663
bo0tzz <git@bo0tzz.me>
2026-06-23 20:10:35 +0200

    fix: zipline json

--- scripts/zipline.sh
+++ scripts/zipline.sh
@@ -1,9 +1,8 @@
 #!/bin/bash
-export QT_ENABLE_HIGHDPI_SCALING=0
 flameshot gui -r > /tmp/ss.png;if [ ! -s /tmp/ss.png ]; then
   exit 1
 fi
 token=$(cat $HOME/.config/i3/scripts/zipline-token)
-curl -H "authorization: $token" https://bo0.tz/api/upload -F file=@/tmp/ss.png -H "Content-Type: multipart/form-data" -H "Format: random" | jq -r '.files[0]' | tr -d '\n' | xsel -ib;
+curl -H "authorization: $token" https://bo0.tz/api/upload -F file=@/tmp/ss.png -H "Content-Type: multipart/form-data" -H "Format: random" | jq -r '.files[0].url' | tr -d '\n' | xsel -ib;
 
 notify-send zipline "screenshot uploaded"
